6a5916de4824b58ed73960c003c6a7345d1dd5df
Aviaifr <Avihaif@gmail.com>
2016-12-03 22:45:56 +0200

    More refactoring
    by Guy's code

--- A17 Ex01 AvihaiFranco 201665940/A17 Ex01 Avihai 201665940/Background.cs
+++ A17 Ex01 AvihaiFranco 201665940/A17 Ex01 Avihai 201665940/Background.cs
@@ -6,15 +6,13 @@ namespace A17_Ex01_Avihai_201665940
 {
     public class Background : SpaceObject
     {
-        public Background(Game i_Game, string i_TextureString) : base(i_Game,i_TextureString)
+        public Background(Game i_Game, string i_TextureString) : base(i_Game, i_TextureString)
         {
-            Initialize(new Vector2(0, 0), Color.Gray, new Vector2(0,0));
+            Initialize(new Vector2(0, 0), Color.Gray, Vector2.Zero);
         }
 
         public override void Update(GameTime i_GameTime)
         {
-
         }
-
     }
 }
--- A17 Ex01 AvihaiFranco 201665940/A17 Ex01 Avihai 201665940/Enemy.cs
+++ A17 Ex01 AvihaiFranco 201665940/A17 Ex01 Avihai 201665940/Enemy.cs
@@ -6,12 +6,16 @@ using Microsoft.Xna.Framework.Input;
 namespace A17_Ex01_Avihai_201665940
 {
     public delegate void WallHitHandler(SpaceObject i_ObjectHitTheWall, float i_OffsetFromHit);
+
     public class Enemy : SpaceObject
     {
         public int Value { get; set; }
+
         public event WallHitHandler m_WallHit;
+
         private float m_timeSinceMoved;
         private float m_TimeBetweenJumps;
+
         public Enemy(Game i_Game, string i_TextureLocation, int i_Value)
             : base(i_Game, i_TextureLocation)
         {
@@ -31,7 +35,7 @@ namespace A17_Ex01_Avihai_201665940
             m_timeSinceMoved += (float)i_GameTime.ElapsedGameTime.TotalSeconds;
             if (m_timeSinceMoved >= m_TimeBetweenJumps)
             {
-                m_Position.X += (m_Speed.X) * m_timeSinceMoved;
+                m_Position.X += m_Speed.X * m_timeSinceMoved;
                 m_timeSinceMoved = m_timeSinceMoved - m_TimeBetweenJumps;
             }
 
@@ -40,16 +44,17 @@ namespace A17_Ex01_Avihai_201665940
                 if (m_WallHit != null)
                 {
                     float offset = (m_Speed.X > 0) ? m_Game.GraphicsDevice.Viewport.Width - (m_Position.X + m_Texture.Width)  : -m_Position.X;
-                    m_WallHit.Invoke(this,offset);
+                    m_WallHit.Invoke(this, offset);
                 }
             }
         }
+
         public void ChangeDirection(float i_MovementXOffset)
         {
             m_Speed.X *= -1;
             m_Position.X += i_MovementXOffset;
             m_Position.Y += m_Texture.Width / 2;
-            m_TimeBetweenJumps = m_TimeBetweenJumps - m_TimeBetweenJumps * 0.04f;
+            m_TimeBetweenJumps = m_TimeBetweenJumps - (m_TimeBetweenJumps * 0.04f);
         }
     }
 }
