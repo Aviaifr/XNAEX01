fe3974fd46b2ad0a29df56aab138782d9e4b8153
Aviaifr <Avihaif@gmail.com>
2017-01-01 22:00:06 +0200

    another fix
    forgot in prev commit

--- A17 Ex01 AvihaiFranco 201665940/A17 Ex01 Avihai 201665940/Wall.cs
+++ A17 Ex01 AvihaiFranco 201665940/A17 Ex01 Avihai 201665940/Wall.cs
@@ -13,7 +13,7 @@ namespace Space_Invaders
 {
     public class Wall : Sprite, ICollidable2D
     {
-        private List<int> m_indexesToDelete;
+        private Rectangle m_RectangleToErase;
         
         public Wall(Game i_Game, string i_TextureString)
             : base(i_Game, i_TextureString)
@@ -21,11 +21,6 @@ namespace Space_Invaders
             m_Tint = Color.White;
         }
 
-        public override void Initialize()
-        {
-            m_indexesToDelete = new List<int>();
-            base.Initialize();
-        }
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
@@ -33,42 +28,34 @@ namespace Space_Invaders
         
         public override void Collided(ICollidable i_Collidable)
         {
-            foreach (int index in m_indexesToDelete)
+            for (int i = 0; i < m_TextureColorData.Length; i++)
             {
-                m_TextureColorData[index].A = 0;
+                Point indexToPoint = new Point((int)m_Position.X + (i % m_Texture.Width), (int)m_Position.Y + i / m_Texture.Width);
+                if (m_RectangleToErase.Contains(indexToPoint))
+                {
+                    m_TextureColorData[i].A = 0;
+                }
             }
-
             this.m_Texture.SetData(m_TextureColorData);
-            m_indexesToDelete.Clear();
         }
 
         public override bool IsPixelBasedCollision(ICollidable i_Source)
         {
             bool collided = false;
-            float collidingHeight = (i_Source as ICollidable2D).Bounds.Height * 0.45f;
-            float directionFactor = (i_Source as Sprite).Velocity.Y / Math.Abs((i_Source as Sprite).Velocity.Y);
             ICollidable2D source = i_Source as ICollidable2D;
-            for (int i = 0; i < m_TextureColorData.Length; i++)
+            int collidierHeight = source.Bounds.Height;
+            int directionFactor = (int)((i_Source as Sprite).Velocity.Y / Math.Abs((i_Source as Sprite).Velocity.Y));
+            for (int i = 0; i < m_TextureColorData.Length && !collided; i++)
             {
                 if (m_TextureColorData[i].A != 0)
                 {
                     Point CollidablePointOnScreen = new Point((int)m_Position.X + (i % m_Texture.Width), (int)m_Position.Y + i / m_Texture.Width);
                     if (source.IsPointInScreenIsColidablePixel(CollidablePointOnScreen))
                     {
+                        m_RectangleToErase = source.Bounds;
                         if (i_Source is SpaceBullet)
                         {
-                            for (int j = 0; j < collidingHeight; j++)
-                            {
-                                int pixToDeletPos = i + (int)(j * directionFactor * this.Width);
-                                if (pixToDeletPos >= 0 && pixToDeletPos < m_TextureColorData.Length)
-                                {
-                                    m_indexesToDelete.Add(pixToDeletPos);
-                                }
-                            }
-                        }
-                        else
-                        {
-                            m_indexesToDelete.Add(i);
+                            m_RectangleToErase.Y += directionFactor * (int)(0.45f * collidierHeight);
                         }
 
                         collided = true;
